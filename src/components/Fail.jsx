f473ba68b0e2c111f7a6f7c7b3302dfa5361fcac
IgorKoltakov <igor.koltakov@icloud.com>
2023-04-11 21:47:38 +0300

    first commit

---
+++ src/components/Fail.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function Fail () {
+  return (
+    <div className="loading">
+      <h1>
+        City not found
+      </h1>
+    </div>
+  )
+}
+
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const API_KEY = 'b2317790f92df034d9878a8616aaa608';
+
+const API_URL = `https://api.openweathermap.org/data/2.5/forecast`
+
+export {API_KEY, API_URL}
